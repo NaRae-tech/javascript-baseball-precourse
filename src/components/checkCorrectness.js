6a4d1a0c624c1414e8a1183100ea571b3f2b40de
NaRae-tech <narae.dev@gmail.com>
2021-12-17 09:31:03 +0900

    오타 수정

--- src/components/checkCorrectness.js
+++ src/components/checkCorrectness.js
@@ -1,5 +1,5 @@
 import { DecideBallCount } from './decideBallCount.js';
-export function CheckCorrectNess(computerBall, userBall) {
+export function CheckCorrectness(computerBall, userBall) {
   const decision = DecideBallCount(computerBall, userBall);
   return (decision[0] === 3 && decision[1] === 0) ? true : false;
 }
