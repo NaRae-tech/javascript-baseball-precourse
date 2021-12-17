c53d2e5ef6ec4e2fe5f8c9ad75719906aca9b92a
NaRae-tech <narae.dev@gmail.com>
2021-12-17 12:01:56 +0900

    만들어놓은 components들 순차 실행

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import { $ } from './components/dom.js';
+import CheckUserInputValidity from './components/checkUserInputValidity.js';
+import { MakeRandomNumber } from './components/makeRandomNumber.js';
+import { PrintResultWords } from './components/printResultWords.js';
+
+function start() {  
+  $('#result').innerHTML = '';
+  $('#submit').type = "button";
+}
 export default function BaseballGame() {
-  
+  start();
+  const computerNumber = MakeRandomNumber();
+  $('#submit').addEventListener('click', () => {
+    const userInput = $('#user-input').value;
+    if (CheckUserInputValidity(userInput)) {
+      PrintResultWords(computerNumber, userInput);
+    }
+  })
 }
 BaseballGame();
