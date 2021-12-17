9e90167032f85315d2902db9296b1ba3774cdaa6
NaRae-tech <narae.dev@gmail.com>
2021-12-17 09:08:35 +0900

    판정 결과를 문장으로 나타냄

---
+++ src/components/decideResultWords.js
@@ -0,0 +1,15 @@
+export function DecideResultWords(computerBall,userBall) {
+  const ballCount = DecideBallCount(computerBall, userBall);  //[strike,ball]순서로 return
+  if (ballCount[0] === 0 && ballCount[1] === 0) {
+    return "낫싱";
+  }
+  else if (ballCount[0] === 0 && ballCount[1] > 1) {
+    return `${ballCount[1]}볼`;
+  }
+  else if (ballCount[0] > 1 && ballCount[1] === 0) {
+    return `${ballCount[0]}스트라이크`
+  }
+  else {
+    return `${ballCount[1]}볼 ${ballCount[0]}스트라이크`;
+  }
+}
