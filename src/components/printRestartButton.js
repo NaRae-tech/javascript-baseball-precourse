8d7ec1a675c059fa144ec8176f3d8c99cff4c9d3
NaRae-tech <narae.dev@gmail.com>
2021-12-17 12:00:22 +0900

    eventlistener문에서 적용함수 오타 수정

--- src/components/printRestartButton.js
+++ src/components/printRestartButton.js
@@ -3,5 +3,5 @@ import Restart from './restart.js';
 export function PrintRestartButton() {
   $('#result').innerHTML +=
     `<p>게임을 새로 시작하시겠습니까?<button id="game-restart-button">게임 재시작</button></p>`;
-  $('#game-restart-button').addEventListener('click', Restart());
+  $('#game-restart-button').addEventListener('click', Restart);
 }
